4a08e908260b84fe0db47c8b52c9f899fe92458c
Hayoung Gam <hahhyoung@gmail.com>
2021-03-21 01:29:15 +0900

    Background.js done!

--- js/background.js
+++ js/background.js
@@ -1,1 +1,12 @@
 const background = document.querySelector("#js-background");
+
+const setBackground = () => {
+  let backgroundNum = Math.floor(Math.random() * 7) + 1;
+  background.style.backgroundImage = `url(../img/${backgroundNum}.jpg)`;
+}
+
+const backgroundInit = () => {
+  setBackground();
+}
+
+backgroundInit();
