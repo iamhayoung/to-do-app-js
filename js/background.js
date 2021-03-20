b55bf671cc5f352e6f1addd0bb3fa809a548ebc9
Hayoung Gam <hahhyoung@gmail.com>
2021-03-21 01:49:59 +0900

    Add background images

--- js/background.js
+++ js/background.js
@@ -1,7 +1,7 @@
 const background = document.querySelector("#js-background");
 
 const setBackground = () => {
-  let backgroundNum = Math.floor(Math.random() * 7) + 1;
+  let backgroundNum = Math.floor(Math.random() * 27) + 1;
   background.style.backgroundImage = `url(../img/${backgroundNum}.jpg)`;
 }
 
