eb1f8c928c42072a0f7dd741010e2f6d3b70063f
Hayoung Gam <hahhyoung@gmail.com>
2021-03-20 21:27:34 +0900

    Clock done.

---
+++ js/clock.js
@@ -0,0 +1,25 @@
+const outputHours = document.querySelector("#js-hours"),
+  outputMins = document.querySelector("#js-mins"),
+  outputSecs = document.querySelector("#js-secs");
+
+const updateClock = () => {
+  const now = new Date();
+  let hours = now.getHours();
+  let mins = now.getMinutes();
+  let secs = now.getSeconds();
+
+  outputHours.innerText = hours < 10 ? `0${hours}` : `${hours}`;
+  outputMins.innerText = mins < 10 ? `0${mins}` : `${mins}`;
+  outputSecs.innerText = secs < 10 ? `0${secs}` : `${secs}`;
+}
+
+const setClock = () => {
+  window.setInterval(updateClock, 1000);
+}
+
+const init = () => {
+  updateClock();
+  setClock();
+}
+
+init();
