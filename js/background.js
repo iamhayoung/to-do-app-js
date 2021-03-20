2ba2f091d752502a20deaf87eeb71a108c2c41cc
Hayoung Gam <hahhyoung@gmail.com>
2021-03-21 03:53:03 +0900

    Fix background url

--- js/background.js
+++ js/background.js
@@ -2,7 +2,7 @@ const background = document.querySelector("#js-background");
 
 const setBackground = () => {
   let backgroundNum = Math.floor(Math.random() * 27) + 1;
-  background.style.backgroundImage = `url(../img/${backgroundNum}.jpg)`;
+  background.style.backgroundImage = `url(img/${backgroundNum}.jpg)`;
 }
 
 const backgroundInit = () => {
