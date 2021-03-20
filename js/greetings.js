fcfc41e1369786e61af2d2882f09c7aeae88d0be
Hayoung Gam <hahhyoung@gmail.com>
2021-03-20 22:24:26 +0900

    Set the username in local storage

--- js/greetings.js
+++ js/greetings.js
@@ -2,13 +2,28 @@ const greetingsForm = document.querySelector("#js-greetingsForm"),
   greetingsInput = document.querySelector("#js-greetingsInput"),
   greetingsText = document.querySelector("#js-greetingsText");
 
+const printGreetings = (userNameValue) => {
+  greetingsText.innerText = `Hello, ${userNameValue}🙌`;
+  greetingsText.classList.add("showing");
+}
+
+const setUserNameLocalStorage = (userNameValue) => {
+  // 로컬스토리지 userName에 값이 있을때만 값을 셋팅하기.
+  localStorage.setItem('userName', userNameValue);
+
+  // 만약에 로컬스토리지userName에 값이 있으면 보여주게 하기
+  greetingsForm.classList.remove("showing");
+  printGreetings(userNameValue);
+}
+
 const handleGreetingsSubmit = (event) => {
   event.preventDefault();
-  let greetingsValue = greetingsInput.value;
-  greetingsValue = "";
+  let userNameValue = greetingsInput.value;
+  setUserNameLocalStorage(userNameValue);
 }
 
 const greetingsInit = () => {
+  // 로컬스토리지 userName에 값이 있으면 첫화면은 인사말띄우기
   greetingsForm.addEventListener("submit", handleGreetingsSubmit)
 }
 
