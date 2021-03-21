79fafa182c2873a2d3d32e742d24023f325b380c
Hayoung Gam <hahhyoung@gmail.com>
2021-03-21 16:55:48 +0900

    Create todo elements

--- js/todo.js
+++ js/todo.js
@@ -1,14 +1,42 @@
 const todoForm = document.querySelector("#js-todoForm"),
-  todoInput = document.querySelector("#js-todoInput");
+  todoInput = document.querySelector("#js-todoInput"),
+  todoList = document.querySelector("#js-todoList");
+
+// !투두 체크
+
+// !투두 삭제
+
+const printTodos = (todos) => {
+  console.log(todos)
+  // li생성
+  const li = document.createElement("li");
+  const div = document.createElement("div");
+  const label = document.createElement("label");
+  const checkbox = document.createElement("input");
+  const delBtn = document.createElement("span");
+  todoList.appendChild(li);
+  li.appendChild(div);
+  div.append(label, checkbox, delBtn);
+  div.classList.add("todo__checkbox");
+  delBtn.classList.add("todo__del-btn");
+  label.setAttribute("for", "todoCheck");
+  checkbox.setAttribute("id", "todoCheck");
+  checkbox.setAttribute("type", "checkbox");
+  delBtn.innerHTML = "<i class='fas fa-times'></i>";
+  div.innerText = todos; // div에 todo텍스트 출력
+
+  // div의 자식 label, checkbox
+}
 
 const setTodosLocalStorage = (todos) => {
-  console.log(todos)
+  localStorage.setItem("toDo", todos);
+  printTodos(todos);
 }
 
 const handleTodoSubmit = (event) => {
   event.preventDefault();
   let todos = todoInput.value;
-  // todo한개 입력하고 나면 todoInput창 비워야함
+  // !todo한개 입력하고 나면 todoInput창 비워야함
 
   setTodosLocalStorage(todos);
 }
