994fbe508bf3899a43b1a9c78c4a0922327b41c1
Hayoung Gam <hahhyoung@gmail.com>
2021-03-21 18:08:21 +0900

    Create elements

--- js/todo.js
+++ js/todo.js
@@ -3,11 +3,18 @@ const todoForm = document.querySelector("#js-todoForm"),
   todoList = document.querySelector("#js-todoList");
 
 let TODOS_ARRAY = [];
+let TODOS_OBJECT = [];
 let i = 1;
 
 // !투두 체크
+const handleTodoDone = (event) => {
+  console.log(event.target);
+}
 
 // !투두 삭제
+const handleTodoDelete = (event) => {
+  console.log(event.target);
+}
 
 const printTodos = () => {
   // 요소 생성
@@ -16,19 +23,25 @@ const printTodos = () => {
   const label = document.createElement("label");
   const checkbox = document.createElement("input");
   const delBtn = document.createElement("span");
+  // !todo텍스트 출력
+  label.innerText = TODOS_OBJECT.todo;
+
+  // element 출력
   div.classList.add("todo__checkbox");
   delBtn.classList.add("todo__del-btn");
-  label.setAttribute("for", "todoCheck");
-  checkbox.setAttribute("id", "todoCheck");
-  checkbox.setAttribute("type", "checkbox");
   delBtn.innerHTML = "<i class='fas fa-times'></i>";
+  label.setAttribute("for", TODOS_OBJECT.id);
+  checkbox.setAttribute("id", TODOS_OBJECT.id);
+  checkbox.setAttribute("type", "checkbox");
   todoList.appendChild(li);
   li.appendChild(div);
-  div.append(label, checkbox, delBtn);
+  div.append(checkbox, label, delBtn);
 
-  // !todo 출력
-  div.innerText = TODOS_ARRAY;
+  // delBtn클릭했을때 삭제함수 실행
+  delBtn.addEventListener('click', handleTodoDelete);
 
+  // checkbox클릭했을때 Done
+  checkbox.addEventListener('click', handleTodoDone);
 }
 
 const setTodosLocalStorage = () => {
@@ -40,11 +53,12 @@ const handleTodoSubmit = (event) => {
   event.preventDefault();
   let todosValue = todoInput.value;
   let number = i++;
+  TODOS_OBJECT = { "id": number,"todo": todosValue }
 
   //! 페이지 리로드 후 투두입력하면 원래 있던 Todo가 새Todo에 上書きされる
 
   if (todosValue) {
-    TODOS_ARRAY.push(JSON.stringify({ "id": number,"todo": todosValue }));
+    TODOS_ARRAY.push(JSON.stringify(TODOS_OBJECT));
     setTodosLocalStorage();
     todoForm.reset(); // todo 입력하고 submit후에는 todoInput창을 clear
   } else {
